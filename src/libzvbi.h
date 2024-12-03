66a1c9bdf3a7f105a5c02070cc6de0d949513c58
Ileana Dumitrescu <ileanadumitrescu95@gmail.com>
2024-12-03 16:58:59 +0200

    Prepare for 0.2.43 release

--- src/libzvbi.h
+++ src/libzvbi.h
@@ -27,7 +27,7 @@
 
 #define VBI_VERSION_MAJOR 0
 #define VBI_VERSION_MINOR 2
-#define VBI_VERSION_MICRO 42
+#define VBI_VERSION_MICRO 43
 
 #ifdef __cplusplus
 extern "C" {
--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 /* TODO: Generate these based on configure.ac*/
 #define VBI_VERSION_MAJOR 0
 #define VBI_VERSION_MINOR 2
-#define VBI_VERSION_MICRO 42
+#define VBI_VERSION_MICRO 43
